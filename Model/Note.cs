f46bdd8ee0425e3348dd966cf9817b0caebfae85
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-07-12 03:29:55 +0700

    Add Model

---
+++ Model/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+    }
+}
